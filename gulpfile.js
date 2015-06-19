859e76cce1c4aa4e1c9be6aeffddbe307a3e3377
larotta69 <larrotta69@gmail.com>
2015-06-19 13:05:54 -0500

    Header and gulp task automator

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	minifycss = require('gulp-minify-css'),
+	rename  = require('gulp-rename'),
+	concat  = require('gulp-concat'),
+	browserify  = require('gulp-browserify'),
+	gulpif  = require('gulp-if');
+
+var env = 'development';
+var sassDir = './public/sass/*.scss';
+var sassOut = './public/stylesheets';
+
+gulp.task('styles', function () {
+	var config = {};
+	var browser = { browsers: ['> 1%', 'IE 7'] };
+
+	if (env === 'development'){
+	  config.sourceComments = 'map';
+	}
+
+	return gulp.src(sassDir)
+		.pipe( sass( config ) )
+		.pipe( gulpif(env != 'development', minifycss())  )
+		.pipe( gulp.dest(sassOut) );
+});
+
+gulp.task('template', function(){
+    
+});
+
+gulp.task('todo', function(){
+  	gulp.watch('./public/sass/components/*.scss', ['styles']);
+});
+
+gulp.task('default', ['styles', 'template']);
