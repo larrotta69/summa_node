e7a3f9738def4289755e612a3db1eaffb52e63ec
larotta69 <larrotta69@gmail.com>
2015-06-26 14:55:12 -0500

    front end contact, back end services

--- model/db.js
+++ model/db.js
@@ -1,3 +1,3 @@
 var mongoose = require('mongoose');
-//mongoose.connect('mongodb://localhost:22550/summadb');
-mongoose.connect('mongodb://localhost:27017/summadb');
+mongoose.connect('mongodb://localhost:22550/summadb');
+//mongoose.connect('mongodb://localhost:27017/summadb');
