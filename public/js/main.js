4fe23053465de8f1feffe5458cc4fdf865f99d21
larrotta69 <larrotta69@gmail.com>
2015-07-01 15:01:57 -0500

    connections string for mongoDB

--- public/js/main.js
+++ public/js/main.js
@@ -6,8 +6,24 @@ $( document ).ready(function(){
 	});
 	$('.modal-trigger').leanModal();
 
-	google.maps.event.addDomListener(window, 'load', initializeBogota);
-	google.maps.event.addDomListener(window, 'load', initializeBarcelona);
+	
+
+	switch(window.location.pathname){
+		case '/':
+			$('.nav-wrapper li:nth-of-type(1) a').addClass('active');
+			break;
+		case '/equipo':
+			$('.nav-wrapper li:nth-of-type(2) a').addClass('active');
+			break;
+		case '/servicios':
+			$('.nav-wrapper li:nth-of-type(3) a').addClass('active');
+			break;
+		case '/contacto':
+			google.maps.event.addDomListener(window, 'load', initializeBogota);
+			google.maps.event.addDomListener(window, 'load', initializeBarcelona);		
+			$('.nav-wrapper li:nth-of-type(4) a').addClass('active');
+			break;
+	}
 });
  
 function initializeBogota() {
@@ -17,14 +33,14 @@ function initializeBogota() {
 		zoom: 18,
 		mapTypeId: google.maps.MapTypeId.ROADMAP
 		}
-	var map = new google.maps.Map(mapCanvas, mapOptions)
+	var map = new google.maps.Map(mapCanvas, mapOptions);
 }
 function initializeBarcelona() {
-	var mapCanvas = document.getElementById('map-canvas-barcelona');
-	var mapOptions = {
-		center: new google.maps.LatLng(41.379623, 2.140651),
+	var mapCanvasBar = document.getElementById('map-canvas-barcelona');
+	var mapOptionsBar = {
+		center: new google.maps.LatLng(21.1639716, -86.84478990000002),
 		zoom: 18,
 		mapTypeId: google.maps.MapTypeId.ROADMAP
 		}
-	var map = new google.maps.Map(mapCanvas, mapOptions)
+	var map = new google.maps.Map(mapCanvasBar, mapOptionsBar);
 }
