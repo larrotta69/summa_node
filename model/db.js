9ddc28f2228e8ecbf3f1b44807d216e49890d1ad
larrotta69 <larrotta69@gmail.com>
2015-07-23 17:31:24 +0000

    merge

--- model/db.js
+++ model/db.js
@@ -4,11 +4,8 @@ var mongoose = require('mongoose');
 //mongoose.connect('mongodb://localhost:27017/summadb');
 
 mongoose.connect("mongodb://summaUserAdmin:1234567@localhost:22550/summadb");
-	var db = mongoose.connection;
-	db.once('open', function () {
-		console.log('MongoDB connection successful.'+ db);
-<<<<<<< HEAD
+var db = mongoose.connection;
+db.once('open', function () {
+	console.log('MongoDB connection successful.'+ db);
 });
-=======
-});
->>>>>>> 92ce662721e5b4dc103ce13e5ed8b08df99e6618
+
