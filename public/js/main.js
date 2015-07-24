115dea5219f28e07ac46ae90e911850f9b057087
larrotta69 <larrotta69@gmail.com>
2015-07-24 11:25:54 -0500

    maps to Bogota and Barcelona

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,5 @@
 $( document ).ready(function(){
+
 	$(".button-collapse").sideNav();
 	$('.slider').slider({
 		full_width: true,
@@ -18,8 +19,6 @@ $( document ).ready(function(){
 
 	});
 
-
-
 	switch(window.location.pathname){
 		case '/':
 			$('.nav-wrapper li:nth-of-type(1) a').addClass('active');
@@ -36,42 +35,10 @@ $( document ).ready(function(){
 			$('.nav-wrapper li:nth-of-type(3) a').addClass('active');
 			break;
 		case '/contacto':
-			google.maps.event.addDomListener(window, 'load', initializeBogota);
-			google.maps.event.addDomListener(window, 'load', initializeBarcelona);
-			google.maps.event.addDomListener(window, 'load', initializeLima);
-
 			$('.nav-wrapper li:nth-of-type(4) a').addClass('active');
 			break;
 	}
 });
-
-function initializeBogota() {
-	var mapCanvas = document.getElementById('map-canvas-bogota');
-	var mapOptions = {
-		center: new google.maps.LatLng(21.1639716, -86.84478990000002),
-		zoom: 18,
-		mapTypeId: google.maps.MapTypeId.ROADMAP
-		}
-	var map = new google.maps.Map(mapCanvas, mapOptions);
-}
-function initializeBarcelona() {
-	var mapCanvasBar = document.getElementById('map-canvas-barcelona');
-	var mapOptionsBar = {
-		center: new google.maps.LatLng(21.1639716, -86.84478990000002),
-		zoom: 18,
-		mapTypeId: google.maps.MapTypeId.ROADMAP
-		}
-	var map = new google.maps.Map(mapCanvasBar, mapOptionsBar);
-}
-function initializeLima() {
-	var mapCanvasBar = document.getElementById('map-canvas-lima');
-	var mapOptionsBar = {
-		center: new google.maps.LatLng(21.1639716, -86.84478990000002),
-		zoom: 18,
-		mapTypeId: google.maps.MapTypeId.ROADMAP
-		}
-	var map = new google.maps.Map(mapCanvasBar, mapOptionsBar);
-}
 
 function getURLParameter(name) {
   return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search)||[,""])[1].replace(/\+/g, '%20'))||null
